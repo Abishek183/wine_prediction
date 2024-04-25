d0a53c26f07a4ca46ca6f7960c09674d81e7d047
Abishek183 <157641088+Abishek183@users.noreply.github.com>
2024-04-25 14:07:07 -0400

    final changes

--- training.py
+++ training.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
 
-    input_path = "TrainingDataset.csv"
-    valid_path = str(sys.argv[1])
+    input_path = "s3://wineprecdit/TrainingDataset.csv"
+    output_path = "s3://wineprecdit/trainedmodel"
 
     print(f"Reading training CSV file from {input_path}")
     df = (spark.read
@@ -38,26 +38,18 @@ if __name__ == "__main__":
     
     train_data_set = clean_data(df)
 
-    print(f"Reading validation CSV file from {valid_path}")
-    df = (spark.read
-          .format("csv")
-          .option('header', 'true')
-          .option("sep", ";")
-          .option("inferschema", 'true')
-          .load(valid_path))
-    
-    valid_data_set = clean_data(df)
-
     all_features = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
                     'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
                     'pH', 'sulphates', 'alcohol', 'quality']
 
+    print("Creating VectorAssembler")
     assembler = VectorAssembler(inputCols=all_features, outputCol='features')
-
+    
+    print("Creating StringIndexer")
     indexer = StringIndexer(inputCol="quality", outputCol="label")
 
+    print("Caching data for faster access")
     train_data_set.cache()
-    valid_data_set.cache()
     
     print("Creating RandomForestClassifier")
     rf = RandomForestClassifier(labelCol='label', 
@@ -71,22 +63,11 @@ if __name__ == "__main__":
     pipeline = Pipeline(stages=[assembler, indexer, rf])
     model = pipeline.fit(train_data_set)
 
-    predictions = model.transform(valid_data_set)
-
-    print("Evaluating the trained model on the validation set")
-    results = predictions.select(['prediction', 'label'])
     evaluator = MulticlassClassificationEvaluator(labelCol='label', 
                                                   predictionCol='prediction', 
                                                   metricName='accuracy')
 
-   
-    accuracy = evaluator.evaluate(predictions)
-    print(f'Accuracy= {accuracy}')
-    
-    metrics = MulticlassMetrics(results.rdd.map(tuple))
-    print(f'f1 score= {metrics.weightedFMeasure()}')
-
-    print("Retraining model using CrossValidator")
+    print("Retraining model on multiple parameters using CrossValidator")
     cvmodel = None
     paramGrid = ParamGridBuilder() \
         .addGrid(rf.maxDepth, [6, 9]) \
@@ -102,17 +83,13 @@ if __name__ == "__main__":
                               evaluator=evaluator,
                               numFolds=2)
 
+    print("Fitting CrossValidator to the training data")
     cvmodel = crossval.fit(train_data_set)
     
+    print("Saving the best model to new param `model`")
     model = cvmodel.bestModel
-    
 
-    predictions = model.transform(valid_data_set)
-    results = predictions.select(['prediction', 'label'])
-    accuracy = evaluator.evaluate(predictions)
-    print(f'Accuracy (after CrossValidation) = {accuracy}')
-    
-    metrics = MulticlassMetrics(results.rdd.map(tuple))
-    print(f'f1 score (after CrossValidation) = {metrics.weightedFMeasure()}')
-
+    print("Saving the best model to S3")
+    model_path = output_path
+    model.write().overwrite().save(model_path)
     spark.stop()
